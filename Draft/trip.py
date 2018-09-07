8b0609710a0f61bd131de3ff0ea9488fd8013060
shady1993 <gauravrhlla19@gmail.com>
2018-09-07 17:45:14 +0530

    Trip Updated

--- Draft/trip.py
+++ Draft/trip.py
@@ -14,6 +14,7 @@ hotel_name = [app.contents[0] for app in soup.select('a.property_title')]
 review_count = [app.contents[0].split()[0] for app in soup.select('a.review_count')]
 rating = [app for app in soup.select('span.ui_bubble_rating')]
 rank = [rank.contents[0].split()[0] for rank in  soup.select('div.popindex')]
+hotel_link = [app['href'] for app in soup.select('a.property_title')]
 hotel_facilities = []
 for ul_tag in soup.select('ul.icons_list'):
 	facility = []
@@ -25,5 +26,13 @@ for ul_tag in soup.select('ul.icons_list'):
 #print review_count
 #print hotel_name
 #print rating
-#print rank  .find('div',{'class':'label'})
 #print hotel_facilities
+#print hotel_link
+url2 = 'https://www.tripadvisor.in' + hotel_link[0]
+response = requests.get(url2)
+soup2 = BeautifulSoup(response.text,'lxml')
+#print soup2
+
+review_header = [app.text for app in soup2.select('span.noQuotes')]
+reviews = [app.text for app in soup2.select('p.partial_entry')]
+#print reviews
